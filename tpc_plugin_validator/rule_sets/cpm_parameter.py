36b5c16ff31cc93490a7452e048791629a0459de
Peter McDonald <me@petermcdonald.co.uk>
2025-09-13 12:54:43 +0100

    Refactored token names and used these for the valid token in rule sets

--- tpc_plugin_validator/rule_sets/cpm_parameter.py
+++ tpc_plugin_validator/rule_sets/cpm_parameter.py
@@ -3,10 +3,14 @@ from tpc_plugin_validator.lexer.tokens.cpm_parameter_validation import \
     CPMParameterValidation
 from tpc_plugin_validator.rule_sets.rule_set import RuleSet
 from tpc_plugin_validator.utilities.severity import Severity
+from tpc_plugin_validator.lexer.utilities.token_name import TokenName
 
 
 class CPMParameter(RuleSet):
     """Handle validation of the parameter validations."""
+
+    CONFIG_KEY='cpm_parameter_validation'
+    VALID_TOKEN_TYPES={TokenName.COMMENT.value, TokenName.CPM_PARAMETER_VALIDATION.value,}
 
     def validate(self) -> None:
         """Validate the parameter validations in the process file."""
@@ -39,32 +43,29 @@ class CPMParameter(RuleSet):
         """
         if token.token_name != 'CPM Parameter Validation':
             return
+
+        allowed_missing: set[str] = {
+            'ProcessFileName',
+            'PromptsFileName',
+        }
+
+        if token.name in allowed_missing:
+            return
+
         for condition in self._prompts_content.get('conditions', {}):
             if condition.token_name != 'Assignment':
+                continue
+            if condition.assigned and f'<{token.name}>' in condition.assigned:
                 return
-            if f'<{token.name}>' in condition.assigned:
-                return
+
         for state in self._process_content.get('states', {}):
-            if f'<{token.name}>' in state.assigned:
+            if state.token_name != 'Assignment':
+                continue
+            if state.assigned and f'<{token.name}>' in state.assigned:
                 return
+
         self._add_violation(
             name='CPMParameterUnusedParameterViolation',
             description=f'The parameter "{token.name}" is validated but is not used, found on line {token.line_number}.',
             severity=Severity.WARNING,
         )
-
-    def _get_config_key(self) -> str:
-        """
-        Property to identify the config key to use for the rule set.
-
-        :return: The config key as a string.
-        """
-        return 'cpm_parameter_validation'
-
-    def _get_valid_token_types(self) -> set[str]:
-        """
-        Provide a set of token types allowed in the section being analysed.
-
-        :return: Set of token types.
-        """
-        return {'Comment', 'CPM Parameter Validation',}
