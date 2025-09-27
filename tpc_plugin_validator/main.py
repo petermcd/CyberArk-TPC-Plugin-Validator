d61bb4cab185cdab0d9cd863abeffeb668df98f4
Peter McDonald <me@petermcdonald.co.uk>
2025-09-27 16:24:51 +0100

    Minor fixes for issues sourcery.ai found

--- tpc_plugin_validator/main.py
+++ tpc_plugin_validator/main.py
@@ -3,7 +3,6 @@
 import argparse
 import os
 
-from tpc_plugin_validator.lexer.lexer import Lexer
 from tpc_plugin_validator.parser.parser import Parser
 from tpc_plugin_validator.validator import Validator
 
@@ -11,17 +10,18 @@ from tpc_plugin_validator.validator import Validator
 def main() -> None:
     """Main entry point for the TPC Plugin Validator module."""
     arg_parse = argparse.ArgumentParser(
-        prog="ProgramName", description="What the program does", epilog="Text at the bottom of help"
+        prog="CyberArk TPC Plugin Validator",
+        description="Validate the provided TPC process and prompts file.",
     )
-    arg_parse.add_argument("process_file", type=str, help="Process file to validate")
-    arg_parse.add_argument("prompts_file", type=str, help="Prompts file to validate")
+    arg_parse.add_argument("process_file", type=str, help="Path to the process file to validate")
+    arg_parse.add_argument("prompts_file", type=str, help="Path to the prompts file to validate")
     args = arg_parse.parse_args()
 
     if not os.path.isfile(args.process_file):
-        raise FileNotFoundError(f'The process file "{args.process_file}" does not exist or is not accessible.')
+        arg_parse.error(f'The process file "{args.process_file}" does not exist or is not accessible.')
 
     if not os.path.isfile(args.prompts_file):
-        raise FileNotFoundError(f'The process file "{args.prompts_file}" does not exist or is not accessible.')
+        arg_parse.error(f'The prompts file "{args.prompts_file}" does not exist or is not accessible.')
 
     with open(args.process_file, "r", encoding="utf-8") as process_handler:
         process_file = process_handler.read()
